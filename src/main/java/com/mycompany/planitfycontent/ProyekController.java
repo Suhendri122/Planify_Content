bc7e0c41022406de8615d65c1df0bc9bf212de4a
Aldio17 <aldio@itbss.ac.id>
2024-05-02 13:35:15 +0700

    penambahan data

--- src/main/java/com/mycompany/planitfycontent/ProyekController.java
+++ src/main/java/com/mycompany/planitfycontent/ProyekController.java
@@ -4,15 +4,21 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
 import java.io.IOException;
+import java.net.URL;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class ProyekController {
+public class ProyekController implements Initializable {
 
     @FXML
     private void bukaHalamanDashboard(ActionEvent event) throws IOException {
@@ -81,5 +87,57 @@ public class ProyekController {
         Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         // Menutup stage (popup)
         stage.close();
+    }
+    
+    @FXML
+    private TableView<Proyek> tableView;
+
+    @FXML
+    private TableColumn<Proyek, String> noColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> namaProyekColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> picProyekColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> namaClientColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> noTeleponColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> hargaColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> tglMulaiColumn;
+
+    @FXML
+    private TableColumn<Proyek, String> tglSelesaiColumn;
+
+    private ObservableList<Proyek> proyekList;
+
+    @Override
+    public void initialize(URL url ResourceBundke resourceBundle) {
+        // Inisialisasi ObservableList
+        proyekList = FXCollections.observableArrayList();
+
+        // Tambahkan data dummy
+        proyekList.add(new Proyek("1", "Proyek A", "PIC A", "Client A", "123456", "$1000", "2024-05-01", "2024-06-01"));
+        proyekList.add(new Proyek("2", "Proyek B", "PIC B", "Client B", "789012", "$2000", "2024-06-01", "2024-07-01"));
+
+        // Set data ke TableView
+        tableView.setItems(proyekList);
+
+        // Hubungkan setiap TableColumn dengan properti data yang sesuai
+        no.setCellValueFactory(cellData -> cellData.getValue().getNo());
+        namaProyek.setCellValueFactory(cellData -> cellData.getValue().getNamaProyek());
+        picProyek.setCellValueFactory(cellData -> cellData.getValue().getPicProyek());
+        namaClient.setCellValueFactory(cellData -> cellData.getValue().getNamaClient());
+        noTelepon.setCellValueFactory(cellData -> cellData.getValue().getNoTelepon());
+        harga.setCellValueFactory(cellData -> cellData.getValue().getHarga());
+        tglMulai.setCellValueFactory(cellData -> cellData.getValue().getTglMulai());
+        tglSelesai.setCellValueFactory(cellData -> cellData.getValue().getTglSelesai());
     }
 }
